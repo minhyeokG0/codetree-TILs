a8d536dd02b1f80ff55d256cbdf380a44b7a8012
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-03-30 20:09:04 +0900

    Title: 두수의 곱과 몫, Time: 193ms, Memory: 14MB, Status: Passed - CodeTree

--- 240330/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
+++ 240330/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -5,5 +5,5 @@ const input = fs.readFileSync(0).toString().trim().split(' ')
 const [a, b] = [Number(input[0]), Number(input[1])]
 
 
-console.log(`${a} * ${b} = ${a*b}
-${a} / ${5} = ${parseInt(a/b)}`)
+console.log(`${a} * ${b} = ${a*b}`)
+console.log(`${a} / ${b} = ${parseInt(a/b)}`)
