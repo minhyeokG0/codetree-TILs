b48b9c016ea36737ed55ee09b88d6539016b5b5b
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-03-31 20:59:20 +0900

    Title: 시험 통과 여부 확인하기, Time: 242ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240331/시험 통과 여부 확인하기/verify-test-passed.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+
+const score = Number(fs.readFileSync(0).toString().trim())
+
+if(score > 80) {
+    console.log('pass')
+} else {
+    console.log(`${80 - score } more score`)
+}
