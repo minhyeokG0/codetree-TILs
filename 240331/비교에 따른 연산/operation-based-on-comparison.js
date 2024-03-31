9d3050484576f670aeaa49ecea13673a4aa6e118
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-03-31 21:24:57 +0900

    Title: 비교에 따른 연산, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240331/비교에 따른 연산/operation-based-on-comparison.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+const input = fs.readFileSync(0).toString().trim().split(' ')
+
+
+const [a, b ] = [Number(input[0]), Number(input[1])]
+
+
+if(a > b) {
+    console.log(a*b)
+    } else{
+        console.log(parseInt(b/a))
+
+}
