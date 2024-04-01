f20934f3b1ffb5cc7a17c1517be0b7c712758a83
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-01 21:37:10 +0900

    Title: 살 수 있는 물건, Time: 248ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240401/살 수 있는 물건/things-able-to-buy.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+const input = Number(fs.readFileSync(0).toString().trim())
+
+
+if(input >= 3000){
+    console.log('book')
+} else if(input >= 1000) {
+    console.log('mask')
+} else {
+    console.log('no')
+}
