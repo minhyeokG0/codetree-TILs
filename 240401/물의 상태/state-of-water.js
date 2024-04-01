d360f7bb88eacfa5799153611e42c53ab3f348dd
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-01 21:31:24 +0900

    Title: 물의 상태, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240401/물의 상태/state-of-water.js
@@ -0,0 +1,12 @@
+const fs =require('fs')
+
+const input = Number(fs.readFileSync(0).toString().trim())
+
+
+if(input < 0 ){
+    console.log('ice')
+} else if(input >= 100){
+    console.log('vapor')
+} else {
+    console.log('water')
+}
