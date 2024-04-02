0aed4797d85d0a05fb43f3d6a1da5b136f08dd62
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-02 13:50:23 +0900

    Title: 장학금, Time: 193ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240402/장학금/scholarship.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const input = fs.readFileSync(0).toString().trim().split(' ')
+
+const [first, second] = [Number(input[0]), Number(input[1])]
+
+if( first < 90 || second < 90) {
+    console.log(0)
+} else if (second >= 95) {
+    console.log(10)
+} else {
+    console.log(5)
+}
