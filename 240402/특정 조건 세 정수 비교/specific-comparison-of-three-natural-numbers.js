10e14280d1c1e45d52c78ecceaa983bcf567142c
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-02 13:00:26 +0900

    Title: 특정 조건 세 정수 비교, Time: 194ms, Memory: 14MB, Status: Passed - CodeTree

--- 240402/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 240402/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -5,7 +5,7 @@ const input = fs.readFileSync(0).toString().trim().split(' ')
 const [a, b, c] = [Number(input[0]), Number(input[1]), Number(input[2])]
 
 const answer1 = a <= b && a <= c ? 1 : 0
-const answer2 = a === b === c ? 1 : 0
+const answer2 = a === b && b === c ? 1 : 0
 
 
 console.log(answer1, answer2)
