4ef46a733404aab7b1abe4354cb7c4db51242b60
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-02 15:20:05 +0900

    Title: 윤년인가, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240402/윤년인가/is-leap-year.js
+++ 240402/윤년인가/is-leap-year.js
@@ -2,4 +2,4 @@ const fs = require('fs')
 
 const y = Number(fs.readFileSync(0).toString().trim())
 
-console.log((y % 4 === 0 || !(y % 100 === 0 && y % 400 !==0)) ? 'true' : 'false')
+console.log(y % 4 === 0 && !(y % 400  === 0) ? 'true' : 'false')
