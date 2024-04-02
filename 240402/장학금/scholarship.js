5ed50503996da8808ceb3adc9670f47fa11aedbe
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-02 13:51:37 +0900

    Title: 장학금, Time: 248ms, Memory: 26MB, Status: Passed - CodeTree

--- 240402/장학금/scholarship.js
+++ 240402/장학금/scholarship.js
@@ -7,7 +7,7 @@ const [first, second] = [Number(input[0]), Number(input[1])]
 if( first < 90 || second < 90) {
     console.log(0)
 } else if (second >= 95) {
-    console.log(10)
+    console.log(100000)
 } else {
-    console.log(5)
+    console.log(50000)
 }
