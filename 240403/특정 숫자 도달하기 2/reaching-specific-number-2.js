3bf2a6eb17a9a5f2d4a08296a9b8d16565b8dafa
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-03 23:52:40 +0900

    Title: 특정 숫자 도달하기 2, Time: 197ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240403/특정 숫자 도달하기 2/reaching-specific-number-2.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const input = Number(fs.readFileSync(0).toString().trim())
+
+
+let result = ''
+
+for(let i = input; i>=1; i--){
+    result += i + ' '
+}
+
+
+console.log(result)
