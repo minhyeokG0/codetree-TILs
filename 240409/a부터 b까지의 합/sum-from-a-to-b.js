08de6411634ec90b0d3fa2305af8e2214c3b6f96
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-09 23:36:01 +0900

    Title: a부터 b까지의 합, Time: 5061ms, Memory: 18MB, Status: Time Limit Exceed - CodeTree

---
+++ 240409/a부터 b까지의 합/sum-from-a-to-b.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+const input = fs.readFileSync(0).toString().trim().split(' ')
+
+const [a, b] = [Number(input[0]), Number(input[1])]
+
+
+let sumVal = 0
+
+for(let i =a; a <= b; i++){
+    sumVal += i
+}
+
+
+console.log(sumVal)
