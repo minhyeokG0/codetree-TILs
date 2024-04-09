8fd45dc6925121ecc152821e45b643fc20940a2c
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-09 21:59:19 +0900

    Title: 배수의 개수 2, Time: 222ms, Memory: 14MB, Status: Passed - CodeTree

--- 240409/배수의 개수 2/number-of-multipliers-2.js
+++ 240409/배수의 개수 2/number-of-multipliers-2.js
@@ -6,7 +6,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n')
 
 let cnt = 0;
 for(let i = 0; i < input.length; i++){
-    if( i % 2 === 1){
+    if( Number(input[i]) % 2 === 1){
         cnt += 1
     }
 }
