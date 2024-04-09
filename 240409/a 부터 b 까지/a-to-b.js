562f93bd108fbedbd65a80d9501cd5e09418c9c8
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-09 20:47:41 +0900

    Title: a 부터 b 까지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240409/a 부터 b 까지/a-to-b.js
+++ 240409/a 부터 b 까지/a-to-b.js
@@ -5,17 +5,15 @@ const input = fs.readFileSync(0).toString().split(' ')
 
 const [a, b] = [Number(input[0]), Number(input[1])]
 
-let result = a+ ' '
-for(let i = a; i < b; i % 2 === 1 ? i *= 2 : i +=3){
-    if(i > b){
-        return
-    } else if( i % 2 === 1){
-        result += (i*2) + ' '
-        
-    } else if ( i % 2 === 0) {
-        result += (i + 3) + ' '
-       
-    } 
+let result = ''
+while(a <= b){
+    result += a + ' '
+    if( a % 2 === 1){
+        a *= 2
+    } else {
+        a += 3
+    }
 }
+
 
 console.log(result)
