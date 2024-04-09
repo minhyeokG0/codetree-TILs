fd9022ace280f48e6f6bc16094b8f92a01c6c015
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-09 20:44:40 +0900

    Title: a 부터 b 까지, Time: 165ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 240409/a 부터 b 까지/a-to-b.js
+++ 240409/a 부터 b 까지/a-to-b.js
@@ -7,13 +7,15 @@ const [a, b] = [Number(input[0]), Number(input[1])]
 
 let result = a+ ' '
 for(let i = a; i < b; i % 2 === 1 ? i *= 2 : i +=3){
-    if( i % 2 === 1){
+    if(i > b){
+        return
+    } else if( i % 2 === 1){
         result += (i*2) + ' '
         
-    } else {
+    } else if ( i % 2 === 0) {
         result += (i + 3) + ' '
        
-    }
+    } 
 }
 
 console.log(result)
