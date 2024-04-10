3de7e93bd287e29e05c8b4059db14ec753060c75
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-04-10 21:06:28 +0900

    Title: 홀수만 더하기, Time: 169ms, Memory: 21MB, Status: Passed - CodeTree

--- 240410/홀수만 더하기/add-only-odd-numbers.js
+++ 240410/홀수만 더하기/add-only-odd-numbers.js
@@ -6,7 +6,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n')
 
 let sum = 0;
 for(let i = 1; i <= input.length; i++){
-    if(Number(input[i]) % 2 === 1 && Number(input[i]) & 3 === 0){
+    if(Number(input[i]) % 2 === 1 && Number(input[i]) % 3 === 0){
         sum += Number(input[i])
 
     }
