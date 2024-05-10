d38a1b017ba62fa4f8b72129dbbfe0d50153ca31
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-05-11 01:24:08 +0900

    Title: 친근하지 않은 수, Time: 305ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240511/친근하지 않은 수/unfriendly-number.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+const input = Number(fs.readFileSync(0).toString().trim())
+
+let count = 0;
+
+for (let i = 1; i <=input; i++){
+    if(i % 2 === 0 || i %3 === 0 || i % 5 ===0){
+        continue
+    }
+    count += 1
+}
+
+console.log(count)
