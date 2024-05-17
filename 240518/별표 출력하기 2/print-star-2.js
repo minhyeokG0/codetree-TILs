67d7b2887e070a3e2b8207a20b65d02a9d53013d
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-05-18 06:46:15 +0900

    Title: 별표 출력하기 2, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240518/별표 출력하기 2/print-star-2.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+const n = Number(fs.readFileSync(0).toString().trim())
+
+
+let str = ''
+
+for(let i = n; i > 0; i--){
+     str = ''
+    for(let j = 0; j < i; j++){
+        str += '* '
+    }
+    console.log(str)
+}
