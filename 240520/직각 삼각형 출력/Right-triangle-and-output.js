547d4557a8fc991460deb80002248575937b612f
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-05-20 02:42:30 +0900

    Title: 직각 삼각형 출력, Time: 174ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240520/직각 삼각형 출력/Right-triangle-and-output.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+const n = Number(fs.readFileSync(0).toString().trim())
+
+
+let str = ''
+
+for(let i = 0; i< n; i++){
+    str = ''
+    for(let j = 0; j < 2*i + 1; j++){
+        str += '*'
+    }
+    console.log(str)
+}
